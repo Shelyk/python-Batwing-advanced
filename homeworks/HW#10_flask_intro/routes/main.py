d02a7c844a7e3510f13ca588b0e557a554a08d5c
Shelyk <shelyk94@gmail.com>
2022-06-21 11:32:17 +0300

    HW#10

--- homeworks/HW#10_flask_intro/routes/main.py
+++ homeworks/HW#10_flask_intro/routes/main.py
@@ -4,7 +4,7 @@ from flask import render_template
 
 @app.route('/')
 def hello_world():
-    a = 'Hello, World!'
+    a = 'Hello, my friend from the Cursor!'
     return render_template('index.html', variable=a)
 
 
