bc9900a726461b821d7838543ffa584093513f57
Shelyk <shelyk94@gmail.com>
2022-06-21 11:22:49 +0300

    HW#10

---
+++ homeworks/HW#10_flask_intro/routes/main.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+def hello_world():
+    a = 'Hello, World!'
+    return render_template('index.html', variable=a)
+
+
+@app.route("/adding/<num1>/<num2>")
+def adding(num1, num2):
+    result = int(num1) + int(num2)
+    return render_template('index.html', variable=result)
