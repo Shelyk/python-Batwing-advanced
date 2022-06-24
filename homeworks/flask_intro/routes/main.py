934b1ac295e6242d7d280a40ae78c9e9f11e022d
Shelyk <shelyk94@gmail.com>
2022-06-24 15:20:44 +0300

    HW11

--- homeworks/flask_intro/routes/main.py
+++ homeworks/flask_intro/routes/main.py
@@ -1,14 +1,91 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, jsonify
+from helpers.file import get_users, write_users
 
 
 @app.route('/')
-def hello_world():
-    a = 'Hello, my friend from the Cursor!'
-    return render_template('index.html', variable=a)
+def main_page():
+    users = get_users()
+    return render_template('index.html', users=users)
 
 
-@app.route("/adding/<num1>/<num2>")
-def adding(num1, num2):
-    result = int(num1) + int(num2)
-    return render_template('index.html', variable=result)
+@app.route('/user-add')
+def user_add():
+    return render_template('user-add.html')
+
+
+@app.route('/users', methods=['POST'])
+def save_user():
+    users = get_users()
+
+    id = 1
+    if len(users) > 0:
+        id = len(users) + 1
+
+    user = {
+        "id": id,
+        'email': request.form.get('email'),
+        'first_name': request.form.get('first_name'),
+        'last_name': request.form.get('last_name'),
+        'work_area': request.form.get('working_area')
+    }
+
+    users.append(user)
+    write_users(users)
+    return redirect("/")
+
+
+@app.route("/user-edit/<int:id>")
+def edit(id):
+    users = get_users()
+
+    for user in users:
+        if user["id"] == id:
+            return render_template("user-add.html", user=user)
+
+    return redirect("/")
+
+
+@app.route("/users/<int:id>", methods=["POST"])
+def update(id):
+    users = get_users()
+
+    for user in users:
+        if user["id"] == id:
+            user["email"] = request.form.get("email")
+            user["first_name"] = request.form.get("first_name")
+            user["last_name"] = request.form.get("last_name")
+            user["work_area"] = request.form.get("working_area")
+
+    write_users(users)
+    return redirect("/")
+
+
+@app.route("/users/delete/<int:id>")
+def delete(id):
+    users = get_users()
+    del users[id - 1]
+    write_users(users)
+    return redirect("/")
+
+
+@app.route("/search")
+def search():
+    email = request.args.get("email")
+    first_name = request.args.get("first_name")
+    last_name = request.args.get("last_name")
+    work_area = request.args.get("work_area")
+
+    users = get_users()
+    found_users = []
+
+    for user in users:
+        if email == user["email"] \
+                or first_name == user["first_name"] \
+                or last_name == user["last_name"] \
+                or work_area == user["work_area"]:
+            found_users.append(user)
+    if found_users:
+         return render_template("search.html", users=found_users)
+    else:
+         return "User not found, enter the correct search data."
