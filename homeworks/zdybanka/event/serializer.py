268fb1063d6d8c51bec24ee38debe7f2593bfaa1
Shelyk Yurii <shelyk94@gmail.com>
2022-07-31 14:50:14 +0300

    HW22

---
+++ homeworks/zdybanka/event/serializer.py
@@ -0,0 +1,28 @@
+from marshmallow import Schema, fields
+from marshmallow_enum import EnumField
+from core.models.user_event import InvitationStatus
+
+from user.serializer import UserSerializer
+
+
+class EventSerializer(Schema):
+    id = fields.Integer(required=True, dump_only=True)
+    name = fields.String(required=True)
+    description = fields.String()
+    starts_at = fields.DateTime()
+    ends_at = fields.DateTime()
+    creator_id = fields.Integer()
+    users = fields.List(fields.Nested(UserSerializer))
+
+
+class EventInvitationSerializer(Schema):
+    users_id = fields.List(fields.Integer)
+
+
+class EventRespondSerializer(Schema):
+    id = fields.Integer(required=True, dump_only=True)
+    name = fields.String(required=True)
+    description = fields.String()
+    starts_at = fields.DateTime()
+    ends_at = fields.DateTime()
+    invitation_status = EnumField(enum=InvitationStatus, by_value=True)
