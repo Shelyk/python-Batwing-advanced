1e526f37ec618bfcc3e9866423206148f200f7ff
Shelyk Yurii <shelyk94@gmail.com>
2022-08-19 00:06:56 +0300

    HW 24 finish

--- homeworks/django_intro/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,11 +0,0 @@
-
-<
-main/apps.py,1/0/10550cb9be3f72f7048fb0806ee3ed9ce8ef5af5
-@
-main/__init__.py,f/4/f4f30c93f0e97bae97ef21bdc0f6c57f06ec39f9
-A
-products/views.py,e/7/e7c0df80dbe407ceab2293cb3ce846b3cc0b1733
-A
-products/admin.py,1/e/1ea3f0cae641cc6a7921f2ebb3dd67fca093dc01
-@
-requirements.txt,1/9/19359a61ae2446b51b549167b014da2fcf265768
--- homeworks/django_intro/main/views.py
+++ homeworks/django_intro/main/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import MenuItem
 
 
 def home(request):
     menu_items = MenuItem.objects.all()
-    return render(request, 'main/index.html', {"menu_items": menu_items})
+    return render(request, "main/index.html", {"menu_items": menu_items})
