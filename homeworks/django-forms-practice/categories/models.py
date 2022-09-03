d09fad69d9e6752da6346fdd914b88387060d21b
Shelyk Yurii <shelyk94@gmail.com>
2022-09-03 10:48:49 +0300

    HW#28 start

--- homeworks/django-forms-practice/categories/models.py
+++ homeworks/django-forms-practice/categories/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
 
 class Category(models.Model):
     cat_title = models.CharField(max_length=255)
-    description = models.TextField()
+    slug = models.CharField(max_length=100, null=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    parent_id = models.IntegerField(null=True, blank=True)
+
 
     def __str__(self):
-        return self.cat_title
+        return self.title
